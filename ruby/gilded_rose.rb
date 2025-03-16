7c36a677c7b6f221b9c7bc027e31a980190c3065
ando-s <armond113212@gmail.com>
2025-03-17 08:47:57 +0900

    リファクタリング: GildedRoseクラスのupdate_qualityメソッドを改善し、ItemBehaviorクラスを導入してアイテムの品質更新を管理

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,96 +1,88 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-      
-      # itemのnameがfooだった場合に、fixmeに更新する
-      item.update_name_from_foo_to_fixme
-    end
+  def update_quality
+    @items = @items.map(&:update_quality)
   end
 end
 
-# 必ず自身のインスタンス変数を操作するよう実装する
-# いいクラスの構成要素
-#   インスタンス変数
-#   完全性を保証するようにインスタンス変数を操作するメソッド
-
-# コンストラクタで初期化する
-# initializeメソッドを実装する
 class Item
-  attr_accessor :sell_in, :quality
-  attr_reader :name
+  attr_reader :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
     @name = name
     @sell_in = sell_in
     @quality = quality
+    @behavior = ItemBehavior.for(name)
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 
-  def update_name_from_foo_to_fixme
-    return if can_change_name_fixme?
-    # @name = "fixme"
-    Item.new(
-      name: "fixme"
-      sell_in: @sell_in
-      quantity: @quality
-    )
-  end
-  
-
-  private
-  
-  def can_change_name_fixme?
-    @name != "foo"
+  def update_quality
+    @behavior.update(self)
   end
 end
+
+class ItemBehavior
+  BEHAVIORS = {
+    "Aged Brie" => "AgedBrieBehavior",
+    "Backstage passes to a TAFKAL80ETC concert" => "BackstagePassBehavior",
+    "Sulfuras, Hand of Ragnaros" => "SulfurasBehavior"
+  }
+
+  def self.for(name)
+    behavior_class = BEHAVIORS.fetch(name, "NormalItemBehavior")
+    Object.const_get(behavior_class).new
+  end
+end
+
+class AgedBrieBehavior
+  def update(item)
+    new_quality = [item.quality + 1, 50].min
+    new_sell_in = item.sell_in - 1
+    Item.new(item.name, new_sell_in, new_quality)
+  end
+end
+
+class BackstagePassBehavior
+  def update(item)
+    new_quality = item.quality + 1
+    new_quality += 1 if item.sell_in < 11
+    new_quality += 1 if item.sell_in < 6
+    new_quality = 0 if item.sell_in <= 0
+    new_quality = [new_quality, 50].min
+    new_sell_in = item.sell_in - 1
+    Item.new(item.name, new_sell_in, new_quality)
+  end
+end
+
+class SulfurasBehavior
+  def update(item)
+    item
+  end
+end
+
+class NormalItemBehavior
+  def update(item)
+    new_quality = item.quality - 1
+    new_quality -= 1 if item.sell_in <= 0
+    new_quality = [new_quality, 0].max
+    new_sell_in = item.sell_in - 1
+    Item.new(item.name, new_sell_in, new_quality)
+  end
+end
+
+# Example usage:
+items = [
+  Item.new("Aged Brie", 10, 20),
+  Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30),
+  Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+  Item.new("Normal Item", 15, 25)
+]
+
+gilded_rose = GildedRose.new(items)
+gilded_rose.update_quality
